13c2995f0787f5aaa1d266854cb59c9064375388
Roberto Santos <robertosantos@Robertos-MBP.PK5001Z>
2018-04-29 12:51:19 -0600

    render chart

--- wildwatcher/src/App.js
+++ wildwatcher/src/App.js
@@ -4,15 +4,46 @@ import Nav from './Nav';
 import Birds from './Birds';
 import Turtles from './Turtles';
 import Form from './Form';
-// import Chart from './Chart';
+import Chart from './Chart';
 
-class App extends Component {
+export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      toggle: true
+      toggle: true,
+      chartData: {}
     };
   }
+
+  componentWillMount() {
+    this.getChartData();
+  }
+
+  getChartData() {
+    this.setState({
+        chartData: {
+            labels: [
+                'Birds',
+                'Turtles'
+            ],
+            datasets: [
+                {
+                    label: 'Sightings',
+                    data: [
+                        23,
+                        16
+                    ],
+                    backgroundColor:[
+                        'rgba(255, 99, 132. 0.6',
+                        'rgba(54, 162, 235, 0.6'
+                    ]
+                }
+            ]
+        }
+      })
+    }
+
+
 
   toggleViewAdd = () => {
     this.setState({
@@ -33,13 +64,11 @@ class App extends Component {
           toggleSearch={this.toggleViewSearch}
           toggleAdd={this.toggleViewAdd}
         />
-        {/* <Chart /> */}
         {this.state.toggle && <Birds />}
         {this.state.toggle && <Turtles />}
         {!this.state.toggle && <Form />}
+        <Chart chartData={this.state.chartData} legendPosition="bottom"/>
       </div>
     );
   }
 }
-
-export default App;
--- wildwatcher/src/Chart.jsx
+++ wildwatcher/src/Chart.jsx
@@ -1,45 +1,40 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import fusioncharts from 'fusioncharts';
-// Load the charts module
-import charts from 'fusioncharts/fusioncharts.charts';
-import reactfusioncharts from 'react-fusioncharts';
-
-// Pass fusioncharts as a dependency of charts
-charts(FusionCharts)
+import { Bar } from 'react-chartjs-2';
 
 export default class Chart extends Component {
-  render() {
-    FusionCharts.ready(function() {
-      var myDataSource = {
-        chart: {
-          caption: 'WildWatcher Tally',
-          subCaption: 'How many animals did you see?',
-          numberPrefix: null,
-          theme: 'ocean'
-        },
-        data: [
-          { label: 'Bakersfield Central', value: '880000' },
-          { label: 'Garden Groove harbour', value: '730000' },
-          { label: 'Los Angeles Topanga', value: '590000' },
-          { label: 'Compton-Rancho Dom', value: '520000' },
-          { label: 'Daly City Serramonte', value: '330000' }
-        ]
-      };
-      var revenueChartConfigs = {
-        id: 'revenue-chart',
-        type: 'column2d',
-        width: '80%',
-        height: 400,
-        dataFormat: 'json',
-        dataSource: myDataSource
-      };
-      ReactDOM.render(
-        <ReactFC {...revenueChartConfigs} />,
-        document.getElementById('chart-container')
-      );
-    });
+    constructor(props) {
+        super(props);
+        this.state = {
+            chartData: props.chartData
+        }
+    }
+            
+    static defaultProps = {
+        displayTitle: true,
+        displayLegend: true,
+        legendPosition: 'right'
+    }
 
-    return <div id="chart-container" />;
-  }
+    render() {
+        return (
+            <div className="chart">
+                <Bar
+                    data={this.state.chartData}
+                    options={{ 
+                        maintainAspectRatio: false,
+                        title: {
+                            display: this.props.displayTitle,
+                            text: 'WildWatch Sightings',
+                            fontSize: 25,
+                            
+                        },
+                        legend: {
+                            display: this.props.displayLegend,
+                            position: this.props.legendPosition
+                        }
+                    }}
+                />
+                </div>
+        )
+    }
 }
