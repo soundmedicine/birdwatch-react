340b60626073f5002f2dcf07a7984507756808d0
Roberto Santos <robertosantos@Robertos-MacBook-Pro.local>
2018-04-30 10:22:40 -0600

    update tests

--- wildwatcher/cypress/integration/app_spec.js
+++ wildwatcher/cypress/integration/app_spec.js
@@ -1,7 +1,12 @@
-describe('WildWatcher ', function () {
-  it('.should() - assert that <title> is correct', function () {
+describe('WildWatcher ', () => {
+  beforeEach(() => {
     cy.visit('http://wildwatcher.surge.sh')
+  })
+  it('.should() - assert that <title> is correct', () => {
     cy.title().should('include', 'WildWatcher')
+    cy.contains('Search').click().should('be', 'active')
+    cy.contains('Add Animal').click().should('have', 'Form')
+    cy.contains('Sightings').click().should('have', 'Chart')
     //   ↲               ↲            ↲
     // subject        chainer      value
   })
